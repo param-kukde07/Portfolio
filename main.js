d8e039e56bbe423cebe671af7ae28186b6aaaed6
paramkukde <paramkukde2@gmail.com>
2023-07-17 11:47:15 +0530

    Modified Few things, Changes Images and improved CSS styling.

---
+++ main.js
@@ -0,0 +1,13 @@
+// const container2 = document.getElementById('js-container');
+// const highlight = document.getElementById('js-highlight');
+// var container2Height;
+
+
+// window.onscroll() {
+//     container2Height = container2.offsetHeight - window.innerHeight;
+//     container2Pos = container2.getBoundingClientRect();
+//     diff = container2Height + container2Pos.top;
+//     progressPercentage = diff / container2Height * 100;
+//     cssWidth = Math.floor(100 - progressPercentage);
+//     highlight.style.width = cssWidth+ "%";
+// }
