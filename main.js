880587c6528ef90e4675ba0e03b6ccc5a67c9a68
Param Kukde <124895048+param-kukde07@users.noreply.github.com>
2023-08-27 12:04:09 +0530

    Update main.js See More/See Less

--- main.js
+++ main.js
@@ -10,4 +10,18 @@
 //     progressPercentage = diff / container2Height * 100;
 //     cssWidth = Math.floor(100 - progressPercentage);
 //     highlight.style.width = cssWidth+ "%";
-// }
+// 
+let btn9=document.getElementsByClassName("btn4")[0]
+let img=document.getElementsByClassName("scroll")[0]
+btn9.addEventListener('click',()=>{
+    if (img.style.overflowY==="hidden") {
+        img.style.overflowY="scroll"
+        // btn9.style.display="none"
+        btn9.innerHTML="See Less"
+    }
+    else{
+        img.style.overflowY="hidden"
+        // img.style.maxHeight="705px"
+        btn9.innerHTML="See More"    
+    }
+})
